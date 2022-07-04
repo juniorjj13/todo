969af2d62149d8488cfc68c7ea729a7990af4403
juniorjj13 <_junior@live.com>
2022-07-04 11:54:52 +0100

    good version

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <input type={"checkbox"}></input>
+      <p className="text">lorem ipsum</p>
+      <button className="delete">X</button>
+    </div>
+  );
+};
+
+export default Card;
