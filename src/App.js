a95e12e0f9955c00cffbc1bae1924a5a7944ccbc
camcoem <94968301+camcoem@users.noreply.github.com>
2022-07-05 12:25:46 +0200

    main code todo app
    Co-authored-by: J <juniorjj13@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import "./App.css";
 import data from "./data.json";
+import React, { useState } from "react";
 
 // components
 import Card from "./components/Card.js";
 import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+import ToDoForm from "./components/ToDoForm";
 
 function App() {
+  const [toDoList, setToDoList] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map((task) => {
+      return task.id === Number(id)
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+    setToDoList(mapped);
+  };
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter((task) => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  };
+
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [
+      ...copy,
+      { id: toDoList.length + 1, task: userInput, complete: false },
+    ];
+    setToDoList(copy);
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="container">
         <Card />
       </div>
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleFilter={handleFilter}
+      />
+      <ToDoForm addTask={addTask} />
     </div>
   );
 }
