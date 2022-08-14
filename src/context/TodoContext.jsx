2f0fa6ace91c6ead6a585e79fdf275edc7113946
camcoem <94968301+camcoem@users.noreply.github.com>
2022-08-14 20:05:50 +0200

    partial connection to todo-api

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,11 +1,13 @@
 import { createContext ,useState } from "react";
-import data from "../data.json";
-
+// import data from "../data.json";
 export const TodoContext = createContext();
 
+const data = fetch('http://localhost:3001')
+.then((response) => response.json())
+.then((data) => console.log("hi", data));
 
 const TodoContextProvider = ({children}) => {
-
+    
     const [toDoList, setToDoList ] = useState(data);
     const [view, setView] = useState("all");
    
