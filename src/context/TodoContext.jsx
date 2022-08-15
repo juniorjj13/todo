e34eb5b89c6b1992080c1badc1f116a5b552d41d
juniorjj13 <_junior@live.com>
2022-08-15 14:29:46 +0100

    fecth api (data)

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,16 +1,22 @@
+import { useEffect } from "react";
 import { createContext ,useState } from "react";
 // import data from "../data.json";
 export const TodoContext = createContext();
 
-const data = fetch('http://localhost:3001')
-.then((response) => response.json())
-.then((data) => console.log("hi", data));
+
 
 const TodoContextProvider = ({children}) => {
+    const [toDoList, setToDoList ] = useState([]);
     
-    const [toDoList, setToDoList ] = useState(data);
+    useEffect(() => {
+        fetch('http://localhost:3001')
+        .then((response) => response.json())
+        .then((data) => setToDoList(data));
+    },[]);
+    
     const [view, setView] = useState("all");
-   
+    
+    
 
     return(
     <TodoContext.Provider  value={{ toDoList, setToDoList, view, setView}}>
