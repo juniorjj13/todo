3137128d6e28dfa00efdd8ade843119f8e4cf91d
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-05 13:38:27 +0200

    implemented start btn with isActive Hook

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,31 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 export default function Pomodoro() {
+  const [isActive, setIsActive] = useState(false);
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
   const [displayMessage, setDisplayMessage] = useState(false);
 
   useEffect(() => {
-    let interval = setInterval(() => {
-      clearInterval(interval);
+    // let interval = null;
+    if (isActive) {
+      let interval = setInterval(() => {
+        clearInterval(interval);
 
-      if (seconds === 0) {
-        if (minutes !== 0) {
-          setSeconds(59);
-          setMinutes(minutes - 1);
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59);
+            setMinutes(minutes - 1);
+          } else {
+            let minutes = displayMessage ? 24 : 4;
+            let seconds = 59;
+
+            setSeconds(seconds);
+            setMinutes(minutes);
+            setDisplayMessage(!displayMessage);
+          }
         } else {
-          let minutes = displayMessage ? 24 : 4;
-          let seconds = 59;
-
-          setSeconds(seconds);
-          setMinutes(minutes);
-          setDisplayMessage(!displayMessage);
+          setSeconds(seconds - 1);
         }
-      } else {
-        setSeconds(seconds - 1);
-      }
-    }, 1000);
-  }, [minutes, seconds, displayMessage]);
+      }, 1000);
+    }
+  }, [minutes, seconds, displayMessage, isActive]);
 
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
@@ -34,9 +38,19 @@ export default function Pomodoro() {
     <div className="pomodoro">
       <div className="message">
         {displayMessage && <div>Break time! New session starts in:</div>}
+        <button type="button" className="btn-start">
+          Run
+        </button>
       </div>
       <div className="timer">
         {timerMinutes}:{timerSeconds}
+        <button
+          type="button"
+          className="btn-start"
+          onClick={() => setIsActive(true)}
+        >
+          Start
+        </button>
       </div>
     </div>
   );
