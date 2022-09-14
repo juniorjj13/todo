e24f227f6575f36afb188b2dca59369d7a097109
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-14 18:32:12 +0200

    added new blog and code, still firebase connection

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -3,8 +3,29 @@ import { useEffect } from "react";
 import { createContext ,useState } from "react";
 export const TodoContext = createContext();
 
+// from Firebase on how to use onSnapshot
+// db.collection("cities").doc("SF")
+//     .onSnapshot((doc) => {
+//         console.log("Current data: ", doc.data());
+//     });
 
 
+// from https://www.freakyjolly.com/react-firebase-materialui-todo-app-tutorial/#Connect_React_with_Firebase_Application 
+// useEffect(() => {
+//     console.log('useEffect Hook!!!');
+
+//     db.collection('todos').orderBy('datetime', 'desc').onSnapshot(snapshot => {
+//       console.log('Firebase Snap!');
+//       setTodos(snapshot.docs.map(doc => {
+//         return {
+//           id: doc.id,
+//           name: doc.data().todo,
+//           datatime: doc.data().datatime
+//         }
+//       }))
+//     })
+
+//   }, []);
 
 const TodoContextProvider = ({children}) => {
     const [toDoList, setToDoList ] = useState([]);
