07d23057ee78a274495834b5ac6318945d9ccd76
camcoem <94968301+camcoem@users.noreply.github.com>
2022-09-21 15:36:18 +0200

    clean up of unused comments and delete context 2

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { TodoContext } from "../context/TodoContext";
 
 const ToDoForm = () => {
-  const { toDoList, setToDoList, addToDos } = useContext(TodoContext);
+  const { addToDos } = useContext(TodoContext);
   const [userInput, setUserInput] = useState({ task: "", complete: false });
 
   const handleSubmit = (e) => {
