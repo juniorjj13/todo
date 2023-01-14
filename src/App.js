b2f7fcfb1140085a31bf94532751b85005d8b8ce
camcoem <94968301+camcoem@users.noreply.github.com>
2023-01-14 18:11:13 +0100

    added and imported Footer Component to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ToDoList from "./components/ToDoList";
 import ToDoForm from "./components/ToDoForm";
 import Pomodoro from "./components/Pomodoro";
 import Overview from "./components/Overview";
+import Footer from "./components/Footer";
 
 // context
 import TodoContextProvider from "./context/TodoContext";
@@ -21,6 +22,7 @@ function App() {
         <Pomodoro />
         <ToDoForm />
         <ToDoList />
+        <Footer />
       </TodoContextProvider>
     </div>
   );
