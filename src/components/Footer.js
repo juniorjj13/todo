25322b1bdd80fa957b508c65974df7cbe0bcf4cc
J <62919997+juniorjj13@users.noreply.github.com>
2023-01-15 12:19:23 +0000

    added thebarista.dev and jrdev.live links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,10 @@ export default function Footer() {
       <a href="https://github.com/juniorjj13/todo" target="_blank">
         <i className="devicon-github-original"></i>
       </a>
+      <br />
+      <a href="https://thebarista.dev">TheBarista Dev</a>
+      <br />
+      <a href="https://jrdev.live">Jr Dev</a>
     </div>
   );
 }
