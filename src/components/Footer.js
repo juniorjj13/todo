03db2e5ba70df79f7fd95b1b2e29dbafb342992b
camcoem <94968301+camcoem@users.noreply.github.com>
2023-01-15 13:57:58 +0100

    added target="_blank" and CN for the Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,19 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footerContainer">
-      <a href="https://jrdev.live">Jr Dev</a>
-      <a href="https://github.com/juniorjj13/todo" target="_blank">
+      <a href="https://jrdev.live" target="_blank" className="footerLinks">
+        Jr Dev
+      </a>
+      <a
+        href="https://github.com/juniorjj13/todo"
+        target="_blank"
+        className="footerLinks"
+      >
         <i className="devicon-github-original"></i>
       </a>
-      <a href="https://thebarista.dev">TheBarista Dev</a>
+      <a href="https://thebarista.dev" target="_blank" className="footerLinks">
+        TheBarista Dev
+      </a>
     </div>
   );
 }
