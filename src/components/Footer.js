203e0965f783bf58370699ae1e52b1b59c8fae1d
camcoem <94968301+camcoem@users.noreply.github.com>
2023-01-17 15:39:10 +0100

    added span around links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,22 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footerContainer">
-      <a href="https://jrdev.live" target="_blank" className="footerLinks">
-        Jr Dev
-      </a>
-      <a
-        href="https://github.com/juniorjj13/todo"
-        target="_blank"
-        className="footerLinks"
-      >
-        <i className="devicon-github-original"></i>
-      </a>
-      <a href="https://thebarista.dev" target="_blank" className="footerLinks">
-        TheBarista Dev
-      </a>
+      <span className="footerLinks">
+        <a href="https://jrdev.live" target="_blank">
+          Jr Dev
+        </a>
+      </span>
+      <span className="footerLinks">
+        <a href="https://github.com/juniorjj13/todo" target="_blank">
+          <i className="devicon-github-original"></i>
+        </a>
+      </span>
+
+      <span className="footerLinks">
+        <a href="https://thebarista.dev" target="_blank">
+          TheBarista Dev
+        </a>
+      </span>
     </div>
   );
 }
